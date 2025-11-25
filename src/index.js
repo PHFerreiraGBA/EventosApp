8824ee2d2031f062fa3ec047dc81c583890e6c20
Pedro Henrique Ferreira <phferreiragba@gmail.com>
2025-11-25 14:52:51 -0300

    Adicionar arquivos da atividade

---
+++ src/index.js
@@ -0,0 +1,9 @@
+let contadorImagem1 = 0;
+let contadorImagem2 = 0;
+function handleMonitorarMouse(codigoImagem) {
+    if(codigoImagem == 1){
+        contadorImagem1++;
+    } else if (codigoImagem = 2) {
+        contadorImagem2++;
+    }
+}
