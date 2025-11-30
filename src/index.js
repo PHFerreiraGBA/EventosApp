6247b34b9303c0db8fd21ce0c22ad9a389da841e
Pedro Henrique Ferreira <phferreiragba@gmail.com>
2025-11-30 16:16:07 -0300

    Adicionar evento no JS e criar JSON com os contadores

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 let contadorImagem1 = 0;
 let contadorImagem2 = 0;
+let floresta = document.getElementById("1")
+let cachoeira = document.getElementById("2")
+
 function handleMonitorarMouse(codigoImagem) {
     if(codigoImagem == 1){
         contadorImagem1++;
     } else if (codigoImagem = 2) {
         contadorImagem2++;
     }
+
+    const contaPonteiroNaImagem = {
+        imagem1: contadorImagem1,
+        imagem2: contadorImagem2
+    }
+
+    return JSON.stringify(contaPonteiroNaImagem)
 }
+
+floresta.addEventListener("mouseover", () => handleMonitorarMouse(1))
+cachoeira.addEventListener("mouseover", () => handleMonitorarMouse(2))
